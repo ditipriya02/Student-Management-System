9c03a6a4c6acf3d2428f7cc02b0ba829dd04845f
trisha2304 <146662969+trisha2304@users.noreply.github.com>
2026-01-17 01:19:38 +0530

    Update StudentDAO.java

---
+++ src/StudentDAO.java
@@ -0,0 +1,22 @@
+public class StudentDAO {
+
+    public void addStudent() {
+        // logic later
+    }
+
+    public void getAllStudents() {
+        // logic later
+    }
+
+    public void getStudentByRoll() {
+        // logic later
+    }
+
+    public void updateStudent() {
+        // logic later
+    }
+
+    public void deleteStudent() {
+        // logic later
+    }
+}
